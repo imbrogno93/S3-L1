609a143e42a90fc6268352dafac9be3956765621
imbrogno93 <145666186+imbrogno93@users.noreply.github.com>
2023-10-09 16:26:19 +0200

    Update perimetro.py

--- perimetro.py
+++ perimetro.py
@@ -8,17 +8,17 @@ def rettangolo(base, altezza):
 	return 2 * (base + altezza)
 
 print("Seleziona la figura geometrica di cui vuoi calcolare il perimetro:\n1 per Quadrato\n2 per Cerchio\n3 per Rettangolo")
-scelta = input("Inserisci la tua scelta: ")
+scelta = float(input("Inserisci la tua scelta: ")
 
-if scelta == "1":
+if scelta == 1:
 	lato = float(input("Inserisci il lato del quadrato: "))
 	perimetro = quadrato(lato)
 	print("Il perimetro del quadrato e': ", perimetro)
-elif scelta == "2":
+elif scelta == 2:
 	raggio = float(input("Inserisci il raggio del cerchio: "))
 	perimetro = cerchio(raggio)
 	print("La circonferenza del cerchio e': ", perimetro)
-elif scelta == "3":
+elif scelta == 3:
 	base = float(input("Inserisci la base del rettangolo: "))
 	altezza = float(input("Inserisci l'altezza del rettangolo: "))
 	perimetro = rettangolo(base, altezza)
